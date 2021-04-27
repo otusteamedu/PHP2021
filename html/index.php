af3f6814440cc8114cc1fe6f397645ac2d5ccab3
Ruslan <russel1987@mail.ru>
2021-04-27 22:57:58 +0300

    added memcached and redis

--- html/index.php
+++ html/index.php
@@ -1,2 +1,19 @@
 <?php
-echo 'Hello world!';
+echo '<p>Hello world!</p>';
+
+$m = new Memcached();
+$m->addServer('memcached',11211);
+$statuses = $m->getStats();
+if($statuses['memcached:11211']['uptime']<1){
+    echo '<p>Memcached is down!</p>';
+}else{
+    echo '<p>Memcached is alive!</p>';
+}
+
+$redis = new Redis();
+if($redis->connect('redis', 6379)) {
+    echo '<p>Redis is alive!</p>';
+}
+else {
+    echo '<p>Redis is down!</p>';
+}
