eee3350a08e9142203ababf8bc50b29d6bc25fa6
Natallia Kazarynava <Natallia_Kazarynava@epam.com>
2021-05-03 16:21:04 +0300

    HW1: delete test logic

--- Homestead/public/index.php
+++ Homestead/public/index.php
@@ -1,13 +1,3 @@
 <?php
-echo "Hello, Otus!</br>";
-try {
-    $conn = new mysqli('localhost', 'homestead', 'secret');
-    $db_list = mysqli_query($conn, "SHOW DATABASES");
-    echo "Databases available: ";
-    while($row = mysqli_fetch_assoc($db_list)) {
-        echo $row["Database"] . " ";
-    }
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
+echo "Hello, Otus!";
 phpinfo();
