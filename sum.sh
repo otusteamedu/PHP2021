8775625bcab726cc9f5b88a2781d09cbddcb0300
Evgeniy Akhtyrskikh <ahtirsk85@gmail.com>
2021-05-16 23:45:45 +0600

    Add function tryNumber

--- sum.sh
+++ sum.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 exp='^[+-]?[0-9]+([.][0-9]+)?$'
-if ! [[ $1 =~ $exp ]] ; then
-   echo "error: First arg not a number" >&2; exit 1
-fi
+function tryNumber {
+  if ! [[ $1 =~ $exp ]]; then
+    echo "error: $2 arg not a number" >&2
+    exit 1
+  fi
+}
 
-if ! [[ $2 =~ $exp ]] ; then
-   echo "error: Second arg not a number" >&2; exit 1
-fi
+tryNumber "$1" 'First'
+tryNumber "$2" 'Second'
 echo "$1 $2" | awk '{print $1+$2}'
