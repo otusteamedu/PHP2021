588f7c637a14ea903635983ccd457881ee378474
Roman <rom4ik.kov@gmail.com>
2021-06-04 18:38:47 +0300

    homework 6 fx

--- ddl.sql
+++ ddl.sql
@@ -1,10 +1,10 @@
 create table halls
 (
-    id       bigint unsigned auto_increment
+    id   bigint unsigned auto_increment
         primary key,
-    name     varchar(255) not null,
-    capacity int          not null
-);
+    name varchar(255) not null comment 'Название зала'
+) collate utf8mb4_unicode_ci;
+
 
 INSERT INTO halls (name, capacity)
 values ('Красный зал', 203),
@@ -15,9 +15,120 @@ create table movies
 (
     id       bigint unsigned auto_increment
         primary key,
-    name     varchar(255) not null,
-    duration time         not null
-);
+    name     varchar(255) not null comment 'Название фильма',
+    duration time         not null comment 'Продолжительность фильма'
+) collate = utf8mb4_unicode_ci;
+
+INSERT INTO movies (name, duration)
+values ('1+1', 01:53:00),
+       ('Начало', 02:42:00),
+       ('Брат', 01:40:00),
+       ('Брат2', 02:07:00),
+       ('Аватар', 02:42:00),
+       ('Титаник', 03:30:00);
+
+
+
+create table orders
+(
+    id        bigint unsigned auto_increment
+        primary key,
+    seance_id bigint unsigned not null,
+    fio       varchar(255) not null comment 'Имя покупателя',
+    create_at datetime     not null,
+    constraint orders_seance_id_foreign
+        foreign key (seance_id) references seances (id)
+            on delete cascade
+) collate = utf8mb4_unicode_ci;
+
+INSERT INTO orders(seance_id, fio, create_at)
+values (1, 'Иван Плюшкин', 2021 - 06 - 01 15:50:16),
+       (3, 'Петр Иванов', 2021 - 06 - 01 16:03:11),
+       (2, 'Алексей Петров', 2021 - 06 - 01 16:03:55),
+       (5, 'Валентин Васильев', 2021 - 06 - 01 16:04:30),
+       (1, 'Евгений Сустов', 2021 - 06 - 01 16:03:55),
+       (2, 'Петр Иванов', 2021 - 06 - 01 16:04:30),
+       (5, 'Иван Плюшкин', 2021 - 06 - 01 15:50:16),
+       (4, 'Петр Иванов', 2021 - 06 - 01 16:03:11);
+
+create table places
+(
+    id       bigint unsigned auto_increment
+        primary key,
+    p_row    int not null,
+    p_number int not null,
+    coef     int not null,
+    hall_id  bigint unsigned not null,
+    constraint places_hall_id_foreign
+        foreign key (hall_id) references halls (id)
+            on delete cascade
+) collate = utf8mb4_unicode_ci;
+
+
+INSERT INTO places(p_row, p_number, coef, hall_id)
+values (1, 1, 1, 1),
+       (1, 2, 1, 1),
+       (2, 1, 1, 1),
+       (2, 2, 1, 1),
+       (3, 1, 1, 1),
+       (3, 2, 1, 1),
+       (4, 1, 1, 1),
+       (1, 1, 1, 2),
+       (1, 2, 1, 2),
+       (2, 1, 1, 2),
+       (2, 2, 1, 2),
+       (3, 1, 1, 2),
+       (3, 2, 1, 2),
+       (4, 1, 1, 2);
+
+create table seances
+(
+    id         bigint unsigned auto_increment
+        primary key,
+    hall_id    bigint unsigned not null,
+    movie_id   bigint unsigned not null,
+    started_at datetime not null,
+    constraint seances_hall_id_foreign
+        foreign key (hall_id) references halls (id)
+            on delete cascade,
+    constraint seances_movie_id_foreign
+        foreign key (movie_id) references movies (id)
+            on delete cascade
+) collate = utf8mb4_unicode_ci;
+
+create
+index seances_hall_id_index
+    on seances (hall_id);
+
+create
+index seances_movie_id_index
+    on seances (movie_id);
+
+INSERT INTO seances(hall_id, movie_id, started_at)
+values (1, 1, 2021 - 06 - 01 15:49:55),
+       (2, 2, 2021 - 06 - 01 16:57:22),
+       (1, 3, 2021 - 06 - 01 11:58:01),
+       (2, 3, 2021 - 06 - 01 11:58:01),
+       (1, 1, 2021 - 06 - 01 11:58:01);
+
+
+create table tickets
+(
+    id       bigint unsigned auto_increment
+        primary key,
+    order_id bigint unsigned not null,
+    place_id bigint unsigned not null,
+    price    double(8, 2
+) not null,
+    constraint tickets_order_id_foreign
+        foreign key (order_id) references orders (id)
+            on delete cascade,
+    constraint tickets_place_id_foreign
+        foreign key (place_id) references places (id)
+            on delete cascade
+) collate = utf8mb4_unicode_ci;
+
+
 
 INSERT INTO movies (name, duration)
 values ('1+1', '01:53:00'),
@@ -33,7 +144,7 @@ create table seances
         primary key,
     hall_id    bigint unsigned not null,
     movie_id   bigint unsigned not null,
-    started_at datetime        not null,
+    started_at datetime not null,
     constraint seances_hall_id_foreign
         foreign key (hall_id) references halls (id)
             on delete cascade,
@@ -54,9 +165,9 @@ create table orders
     id         bigint unsigned auto_increment
         primary key,
     seance_id  bigint unsigned not null,
-    name       varchar(255)    not null,
-    full_price int             not null,
-    create_at  datetime        not null,
+    name       varchar(255) not null,
+    full_price int          not null,
+    create_at  datetime     not null,
     constraint orders_seance_id_foreign
         foreign key (seance_id) references seances (id)
             on delete cascade
@@ -78,16 +189,25 @@ create table tickets
     id       bigint unsigned auto_increment
         primary key,
     order_id bigint unsigned not null,
-    place    int             not null,
-    price    int             not null,
+    place    int not null,
+    price    int not null,
     constraint tickets_order_id_foreign
         foreign key (order_id) references orders (id)
             on delete cascade
 );
 
 INSERT INTO tickets (order_id, place, price)
-values (1, 13, 100),
-       (3, 24, 100),
-       (2, 35, 100),
-       (4, 23, 100),
-       (1, 34, 100);
+values (1, 15, 100),
+       (1, 16, 100),
+       (2, 17, 150),
+       (2, 18, 150),
+       (3, 19, 150),
+       (3, 20, 150),
+       (4, 21, 250),
+       (4, 22, 100),
+       (5, 23, 100),
+       (5, 24, 150),
+       (6, 25, 150),
+       (6, 26, 150),
+       (7, 27, 150),
+       (7, 28, 250);
--- sql_request.sql
+++ sql_request.sql
@@ -1,8 +1,22 @@
---SQL для нахождения самого прибыльного фильма
+--SQL для нахождения самого прибыльного фильма №1
 SELECT seances.movie_id, movies.name, sum(ss.price) as movie_sum
 from seances
          left join movies on seances.movie_id = movies.id
-         left join (SELECT seance_id, SUM(full_price) as price from orders group by seance_id) as ss
+         left join (SELECT seance_id, SUM(t1.t_sum) as price
+                    from orders
+                             left join (SELECT order_id, sum(price) as t_sum
+                                        FROM tickets
+                                        GROUP BY order_id) as t1 ON orders.id = t1.order_id
+                    group by seance_id) as ss
                    on seances.id = ss.seance_id
 group by seances.movie_id
-order by movie_sum desc limit 1;
+order by movie_sum desc;
+
+
+--SQL для нахождения самого прибыльного фильма №2
+SELECT movies.name, sum(price)
+FROM tickets
+         left join orders on tickets.order_id = orders.id
+         left join seances on orders.seance_id = seances.id
+         left join movies on seances.movie_id = movies.id
+group by movies.id;
