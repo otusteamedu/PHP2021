9ad4ac8e085caedc0deeb89d044b83536e57526d
Anton <emets.anton@gmail.com>
2021-06-27 21:38:32 +0300

    HW7

---
+++ view_marketing.sql
@@ -0,0 +1,13 @@
+CREATE VIEW marketing AS SELECT (SELECT title FROM films WHERE attr_values.id_film = id_film) AS title, 
+(SELECT type FROM attr_type WHERE id_type = (SELECT id_type FROM attr WHERE id_attr = attr_values.id_attr)) AS type, 
+(SELECT title FROM attr WHERE id_attr = attr_values.id_attr) AS attr,  
+CASE WHEN data_int IS NOT NULL
+THEN CAST(data_int AS TEXT)
+WHEN data_float IS NOT NULL
+THEN CAST(data_float AS TEXT)
+WHEN data_text IS NOT NULL
+THEN data_text
+WHEN data_date IS NOT NULL
+THEN CAST(data_date AS TEXT)
+END AS value
+FROM attr_values
