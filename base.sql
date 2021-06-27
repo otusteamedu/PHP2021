d4acdb87c5ea9e290b8f939c52e66697a215c812
Anton <emets.anton@gmail.com>
2021-06-27 09:37:39 +0300

    HW6

--- base.sql
+++ base.sql
@@ -2,7 +2,8 @@ CREATE DATABASE cinema;
 USE cinema;
 
 CREATE TABLE schedule (
-    id_hall tinyint NOT NULL PRIMARY KEY,
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_hall tinyint NOT NULL,
     title_id int NOT NULL,
     timestamp int(11) NOT NULL,
     price float NOT NULL
@@ -11,19 +12,22 @@ CREATE TABLE schedule (
 CREATE TABLE tickets (
     id CHAR(255) NOT NULL PRIMARY KEY,
     timestamp int(11) NOT NULL,
-    hall tinyint NOT NULL,
+    id_hall tinyint NOT NULL,
     title_id int NOT NULL,
     row tinyint NOT NULL,
     seat tinyint NOT NULL,
     price float NOT NULL
 );
 
-CREATE TABLE hall1 (
-    row tinyint NOT NULL,
-    seats tinyint NOT NULL
+CREATE TABLE halls_list (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_hall int NOT NULL PRIMARY KEY,
+    title CHAR(255) NOT NULL,
 );
 
-CREATE TABLE titles (
-    id int NOT NULL PRIMARY KEY,
-    title CHAR(255) NOT NULL,
+CREATE TABLE hall (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_hall tinyint NOT NULL,
+    row tinyint NOT NULL,
+    seats tinyint NOT NULL
 );
