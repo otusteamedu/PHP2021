2b2e74bb32646f3a1b4f3a5a332196e1f899e636
igor-roganov1989 <30695538+igor-roganov1989@users.noreply.github.com>
2021-07-09 18:09:41 +0300

    del LF

---
+++ Docker/code/vendor/igorroganov/otus-php-4/src/validator/Validator.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\validator;
+
+class Validator
+{
+    public function IsValidBrackets($brackets):bool {
+
+        $counter = 0;
+        $bracket_str = mb_str_split($brackets);// (()()(()))
+
+        foreach ( $bracket_str as $char ) {
+
+            if ($char == "(") {
+                $counter++;
+            } elseif ($char == ")") {
+                $counter--;
+            } else {
+                throw new \Exception('Передан неверный символ');
+            }
+        }
+        return $counter === 0;
+    }
+}
