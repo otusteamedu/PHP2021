b0eda284ea9b5fd5e834c169be74d24e64140bda
Roman <rom4ik.kov@gmail.com>
2021-07-19 18:12:38 +0300

    homework 4

---
+++ Validation/config/app.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+define('ROOT_PATH', dirname(__DIR__));
+
+define('LAYOUTS_PATH', ROOT_PATH . '/resources/layouts/');
+define('VIEWS_PATH', ROOT_PATH . '/resources/views/');
