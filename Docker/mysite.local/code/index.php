de25005950de83928e3bdc3f5d15ee63b73ff5ca
Alexander Doronichev <alexander.doronichev@mail.ru>
2021-07-21 16:31:46 +0300

    Homework 1: fix2

--- Docker/mysite.local/code/index.php
+++ Docker/mysite.local/code/index.php
@@ -1,32 +1,3 @@
 <?php
-
-require __DIR__ . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-try {
-
-    $connectionString = "mysql:" .
-        "dbname=" . $_ENV["MYSQL_DATABASE"]
-        . ";host=" . $_ENV["MYSQL_HOST"]
-        . ";port=" . $_ENV["MYSQL_PORT"];
-
-    $dbh = new PDO(
-        $connectionString,
-        $_ENV["MYSQL_USERNAME"],
-        $_ENV["MYSQL_PASSWORD"]
-    );
-
-    $testQuery = $dbh->query('SHOW TABLES')->fetchObject();
-    echo "Mysql connection established!";
-
-    $dbh = null;
-
-} catch (PDOException $e) {
-    echo "Mysql connection failed: " . $e->getMessage();
-    die();
-}
-echo "<br />";
-echo "Hello, Otus!!!";
-
-phpinfo();
+    phpinfo();
+    exit();
