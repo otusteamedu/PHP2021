d4ea2ded0412eb7993c3b6d077781fde57056bb6
Oleg Sviridov <olegsv3007@yandex.ru>
2021-08-07 16:52:58 +0200

    Use awk instead of bc in sum script
    - Использование awk вместо bc для первой задачи

--- sum.sh
+++ sum.sh
@@ -2,26 +2,19 @@
 
 #Функция, проверяющая является ли аргумент числом
 function isNumber {
-	if [[ $1 =~ ^\-?[0-9]{1,}(\.[0-9]{1,})?$ ]]
+	if [[ $1 =~ ^\-?[0-9]{1,}(\,[0-9]{1,})?$ ]]
 	then
 		return 0
 	fi
 	return 1
 }
 
-#Проверка наличия пакета bc
-if ! dpkg -l bc &> /dev/null
-then
-    echo "Please install bc!"
-    exit 1
-fi
-
 var1=$1
 var2=$2
 
 if $(isNumber $var1) && $(isNumber $var2) && [[ ${#} -eq 2 ]]
 then
-	sum=$(echo "$var1+$var2" |bc)
+	sum=`echo "$var1 $var2" | awk '{print $1 + $2}'`
 	echo $sum
 	exit 0
 else
