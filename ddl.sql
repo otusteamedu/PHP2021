cc46a18f8f321e1ae6e161c7acfe2d629cdc6b15
Alexander Doronichev <alexander.doronichev@mail.ru>
2021-08-15 11:45:11 +0300

    Homework 7: fix

--- ddl.sql
+++ ddl.sql
@@ -30,13 +30,10 @@ CREATE TABLE IF NOT EXISTS public."filmAttributeValue"
     id serial NOT NULL,
     film_attribute_id smallint NOT NULL,
     film_id integer NOT NULL,
-    value_string character varying(255),
     value_text text,
-    value_integer integer,
     value_numeric numeric(8, 4),
     value_boolean boolean,
     value_timestamp timestamp without time zone,
-    value_interval interval,
     PRIMARY KEY (id)
 );
 
