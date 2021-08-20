8303c6fe0d2792bf5f4d29df582c0c4f7c122db6
repetitor <repetitor202@gmail.com>
2021-08-20 16:26:21 +0300

    hw12-testing - basic app

---
+++ hw12-testing/code/src/Repetitor202/controllers/HelloController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Repetitor202\controllers;
+
+class HelloController
+{
+    public function hello(): array
+    {
+        return ['hello' => 'world'];
+    }
+}
---
+++ hw12-testing/code/src/Repetitor202/routes/Router.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Repetitor202\routes;
+
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\ServerRequest;
+use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use League\Route\Router as LeagueRoute;
+use League\Route\Strategy\JsonStrategy;
+use Symfony\Component\Yaml\Yaml;
+
+class Router
+{
+    private array $routes;
+    private ServerRequest $request;
+    private LeagueRoute $router;
+
+    public function __construct()
+    {
+        $this->routes = Yaml::parseFile(dirname(__DIR__) . '/routes/routing.yml');
+
+        $this->request = ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
+
+        $responseFactory = new ResponseFactory();
+        $strategy = new JsonStrategy($responseFactory);
+
+        $this->router = new LeagueRoute();
+        $this->router->setStrategy($strategy);
+
+        foreach ($this->routes as $route) {
+            $this->router->map(
+                $route['method'],
+                $route['path'],
+                '\Repetitor202\controllers\\' . $route['resource'] . '::' . $route['action']
+            );
+        }
+    }
+
+    public function getResponse(): void
+    {
+        $response = $this->router->dispatch($this->request);
+        (new SapiEmitter())->emit($response);
+    }
+}
