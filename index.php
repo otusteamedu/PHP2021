f6e92469f598c2a0131e8fd259b29e20ca73a1e1
Царев Дмитрий Борисович <d.b.tsarev@ticketland.ru>
2021-08-31 02:05:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
-
 use HelloComponent\HelloComponent;
 
 require('vendor/autoload.php');
-//require('vendor/dtsarev/hellocomponent/HelloComponent/HelloComponent.php');
 
 $say = new HelloComponent();
 $say->sayHello();
-
-//print_r(get_declared_classes());
