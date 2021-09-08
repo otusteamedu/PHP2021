a12893ab60c92f6cf6b0a4b0dce940b92c19cf60
Евгений Шарафетдинов <evgn.ekb@culturavision.com>
2021-09-08 07:40:52 +0500

    Правки

--- code/App/App.php
+++ code/App/App.php
@@ -10,17 +10,7 @@ class App {
     public function run(){
 
         $database = new Databse();
-
         $router = new Router();
-
-        
-        $router->add('', ['controller' => 'Home', 'action' => 'index']);
-        $router->add('messages', ['controller' => 'Messages', 'action' => 'index']);
-        $router->add('{controller}/{action}');
-        
-        $router->dispatch($_SERVER['QUERY_STRING']);
-
-          
 
     }
 
