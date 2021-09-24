761950ca4be512406f7d395cda6731eeb5103d68
Ася Скульская <savi@m18.ru>
2021-09-24 04:49:02 +0300

    required php version update and some refactoring

--- code/src/HW5/App.php
+++ code/src/HW5/App.php
@@ -6,59 +6,49 @@ use Exception;
 
 class App
 {
-	private $emails = [];
+    private $emails = [];
 
-	public function run( array $argv ) : void
-	{
-        if ( empty($argv) )
-        {
-            throw new Exception( 'Please specify a file name to read email list from.' );
+    public function run(array $argv) : void
+    {
+        if (empty($argv)) {
+            throw new Exception('Please specify a file name to read email list from.');
         }
 
         $fn = $argv[1];
-        $this->readEmailsFromFile( $fn );
+        $this->readEmailsFromFile($fn);
 
-		foreach ( $this->emails as $email )
-		{
-			echo $email . ' = ' . ( ( $this->checkEmail( $email) ) ? 'valid' : 'invalid' ) . "\r\n\r\n";
-		}
-	}
-
-	private function readEmailsFromFile( $fn ) : void
-    {
-        $this->emails = file($fn, FILE_IGNORE_NEW_LINES);
-
-        if ( empty( $this->emails ) )
-        {
-            throw new Exception( 'No emails' );
+        foreach ($this->emails as $email) {
+            echo $email . ' = ' . (($this->checkEmail($email)) ? 'valid' : 'invalid') . "\r\n\r\n";
         }
     }
 
-	private function checkEmail( string $email ) : bool
-	{
-		if ( empty( $email ) )
-		{
-			return false;
-		}
-		
-		if ( !filter_var($email, FILTER_VALIDATE_EMAIL) ) 
-		{
-			return false;
-		}
-		
-		$host = explode('@', $email)[1];
-		
-		$getmxrr = getmxrr( $host, $mx_records);
-		
-		if ( !$getmxrr )
-		{
-			return false;
-		}
-		
-		return true;
-	}
-	
-	
+    private function readEmailsFromFile($fn) : void
+    {
+        $this->emails = file($fn, FILE_IGNORE_NEW_LINES);
 
+        if (empty($this->emails)) {
+            throw new Exception('No emails');
+        }
+    }
+
+    private function checkEmail(string $email) : bool
+    {
+        if (empty($email)) {
+            return false;
+        }
+        
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+        
+        $host = explode('@', $email)[1];
+        
+        $getmxrr = getmxrr($host, $mx_records);
+        
+        if (!$getmxrr) {
+            return false;
+        }
+        
+        return true;
+    }
 }
-
