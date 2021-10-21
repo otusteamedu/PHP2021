e9a1d8dd076d9da153f0a16e22502815f12bc318
Владислав <dvovlad@mail.ru>
2021-10-21 16:34:55 +0300

    Create docker-compose config

---
+++ Docker/code/index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello, Otus!";
+
+//phpinfo();
+/*error_reporting(E_ALL & ~E_NOTICE);
+
+$memcached = new Memcached; 
+
+$memcached->addServer('memcached', 11211);
+
+echo '<pre>'; print_r($memcached->getServerList()); echo '</pre>';
+
+if($memcached->getStats() === false) {
+    echo 'returned false';
+} else {
+    echo '<pre>'; print_r($memcached->getStats()); echo '</pre>';
+}*/
