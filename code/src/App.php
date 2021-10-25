e1c49e887cf26559d91ad50fa7a910adc58de592
Stepanov Sergeu <prostoforce1@gmail.com>
2021-10-25 14:19:53 +0300

    ElasticSearch YouTube

--- code/src/App.php
+++ code/src/App.php
@@ -3,6 +3,7 @@
 namespace App;
 use Adding\Adding;
 use Statistics\Statistics;
+use Display\Display;
 
 class App
 {
@@ -13,14 +14,7 @@ class App
 
         $statistics = (new Statistics())->sortAllChannelsStatistics();
 
-        foreach ($statistics as $statistic) {
-            echo "Id канала: " . $statistic['statistics']['id_channel'] . "\n";
-            echo "Рейтинг канала: " . round($statistic['rating'], 2) . "\n";
-            echo "Количество просмотров: " . $statistic['statistics']['all_view_count'] . "\n";
-            echo "Количество подписчиков: " . $statistic['statistics']['subscriber_count'] . "\n";
-            echo "Количество лайков: " . $statistic['statistics']['all_like_count'] . "\n";
-            echo "Количество дизлайков: " . $statistic['statistics']['all_dislike_count'] . "\n\n";
-        }
+        (new Display())->outputResult($statistics);
     }
    
 }
