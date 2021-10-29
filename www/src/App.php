ba5cc56d073da1b5187a5ca9d3690c7fc943be84
raketa_nickolay <nikolay.vasilev@raketa.agency>
2021-10-29 18:01:00 +0300

    in_proccess

--- www/src/App.php
+++ www/src/App.php
@@ -3,15 +3,25 @@ namespace Src;
 
 class App {
 
+    CONST socket = 555;
+
     public function run()
     {
-        $socket = socket_create(AF_UNIX, SOCK_STREAM, 0);//создаём сокет
-        try {
-            socket_bind($socket, '/tmp/socket' . rand() . '.sock');
-        } catch (\Exception $exception) {
-            var_dump($exception);
+//        var_dump(getopt('f:')['f']);
+        if (getopt('f:')['f'] == 'server') {
+            $socket = socket_create(AF_UNIX, SOCK_STREAM, 0);//создаём сокет
+            try {
+                socket_bind($socket, '/tmp/socket' . $this::socket . '.sock');
+            } catch (\Exception $exception) {
+                var_dump($exception);
+            }
+            $result = socket_listen($socket);
+            $connection = socket_accept($socket);
+            do {
+                $message = socket_read($connection, 1024);
+                echo $message;
+            } while ($message != 'выход');
         }
-        $result = socket_listen($socket);
-        $connection = socket_accept($socket);
+
     }
 }
