a36a14facce7687153c6b5b422efb6c09054c7c1
Владислав <dvovlad@mail.ru>
2021-10-30 15:40:30 +0300

    add check bc exist

--- sum.sh
+++ sum.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 INT1=$1
 INT2=$2
+I=`dpkg -s bc | grep "Status" ` #проверяем состояние пакета (dpkg) и ищем в выводе его статус (grep)
+if [ -n "bc" ] #проверяем что нашли строку со статусом (что строка не пуста)
+then
+   echo "$INT1 + $INT2 = " #выводим результат
+else
+   echo "bc not installed"
+fi
 INT=$(echo "$INT1 + $INT2" | bc)
 re='^-?[0-9]+([.][0-9]+)?'
 if ! [[ $1 =~ $re ]] || ! [[ $2 =~ $re ]]; then
