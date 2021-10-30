9e88af205e18e035536034da8fb5062977a247c5
Владислав <dvovlad@mail.ru>
2021-10-30 15:19:18 +0300

    fix for float

--- sum.sh
+++ sum.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 INT1=$1
 INT2=$2
-INT=$(($INT1+$INT2))
-re='^-?[0-9]\d*(\.\d+)?$'
+INT=$(echo "$INT1 + $INT2" | bc)
+re='^-?[0-9]+([.][0-9]+)?'
 if ! [[ $1 =~ $re ]] || ! [[ $2 =~ $re ]]; then
    echo "error: Not a number" >&2; exit 1
 fi
