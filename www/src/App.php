08fe5aa63795addb6612b3560c6213a320990230
raketa_nickolay <hugopochta@gmail.com>
2021-10-30 13:04:14 +0300

    'simple'

--- www/src/App.php
+++ www/src/App.php
@@ -3,10 +3,13 @@ namespace Src;
 
 class App {
 
-    CONST socket = 5346;
+    CONST socket = 123123;
 
     public function run()
     {
+        $client_sockets = [];
+        $abort = false;
+        $NULL = null;
 //        var_dump(getopt('f:')['f']);
         if (getopt('f:')['f'] == 'server') {
             $socket = socket_create(AF_UNIX, SOCK_STREAM, 0);//создаём сокет
@@ -15,23 +18,71 @@ class App {
             } catch (\Exception $exception) {
                 var_dump($exception);
             }
-            $result = socket_listen($socket);
-            $connection = socket_accept($socket);
-            // Поток вывода меняется на консоль
-            do {
-                $message = socket_read($connection, 1024);
-//                socket_write($socket, 'sdfsdf');
-                echo $message;
-            } while (true);
+            socket_listen($socket);
+
+            $read = [$socket];
+
+//            while (!$abort) {
+            while (true) {
+                $num_changed = socket_select($read, $read, $NULL , 0);
+
+                if ($num_changed) {
+//                    if(in_array($socket, $read))
+//                    {
+//                        echo 333;
+                        $client_sockets[]= socket_accept($socket);
+                        echo "Принято подключение (" . count($client_sockets)  . " of clients)\n";
+//                    }
+                }
+
+                foreach($client_sockets as $key => $client)
+                {
+                    $input = socket_read($client, 1024);
+                    if($input === false)
+                    {
+                        socket_shutdown($client);
+                        unset($client_sockets[$key]);
+                    } else {
+                        $input = trim($input);
+
+                        echo $input;
+
+                        if (!socket_write($client, "Вы сказали: $input\n") )
+                        {
+                            socket_close($client);
+                            unset($client_sockets[$key]) ;
+                        }
+                    }
+                    if($input == 'exit')
+                    {
+                        socket_shutdown($socket);
+                        $abort = true;
+                    }
+//                    do {
+////                socket_write($socket, 'sdfsdf');
+//                        echo $input;
+//                    } while (true);
+                }
+//                $connection = socket_accept($socket);
+                // Поток вывода меняется на консоль
+
+                $read[] = $socket;
+            }
+
+
+
         } else {
             $socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
 
             $connection = socket_connect($socket, '/tmp/socket' . $this::socket . '.sock');
-            socket_write($socket, 'hello');
+//            socket_write($socket, 'hello');
 
             do {
                 $a = readline('');
                 socket_write($socket, $a);
+
+                $input = socket_read($socket, 1024);
+                echo $input;
 //                $warn = socket_read($socket, 1024);
 //                echo 123;
             } while (true);
