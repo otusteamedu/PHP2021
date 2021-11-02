ebef1c7d5cc36fa2647708b1d70c4eea3495c2a6
raketa_nickolay <hugopochta@gmail.com>
2021-11-02 14:14:54 +0300

    'finish'

--- www/index.php
+++ www/index.php
@@ -1,4 +1,19 @@
 <?php
-$task = '(()()()()))((((()()()))(()()()(((()))))))';
-$keywords = preg_match("&^([^()]*\((?:[^()]|(?1))*\)[^()]*)+$|^[^()]+?$&", $task);
-var_dump($keywords);
+try {
+    if (empty($string = $_POST['string'])) {
+        throw new Exception();
+    }
+    $isCoincidence = preg_match("&^([^()]*\((?:[^()]|(?1))*\)[^()]*)+$|^[^()]+?$&", $string);
+    if (!$isCoincidence) {
+        throw new Exception();
+    }
+    setStatus(200);
+    echo 'Все хорошо';
+} catch (Exception) {
+    setStatus(400);
+    echo 'Все плохо';
+}
+
+function setStatus($code) {
+    header('Content-Type: text/html; charset=utf-8', false, $code);
+}
