5c118e70a133c85b367e2b7d29eeda3350c2c196
Владислав <dvovlad@mail.ru>
2021-11-03 21:12:18 +0300

    fix

--- sum.sh
+++ sum.sh
@@ -5,4 +5,4 @@ re='^-?[0-9]+([.][0-9]+)?'
 if ! [[ $1 =~ $re ]] || ! [[ $2 =~ $re ]]; then
    echo "error: Not a number" >&2; exit 1
 fi
-awk -v a=$INT1 -v b=$INT2 'BEGIN {print "(a + b) = ", (a + b) }'
+awk -v a=$INT1 -v b=$INT2 'BEGIN {print (a + b) }'
