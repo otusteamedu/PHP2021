553e7a8a9d68399ae5d264aa60b6e5d01009a51d
Minyakova Alina <minyakovaas@mail.ru>
2021-11-09 13:31:58 +0300

    Verification form

---
+++ code/action-form.php
@@ -0,0 +1,84 @@
+<?php
+
+class FormValide{/*Start Class*/
+
+	public $textValide;
+
+	public function __construct($name) {
+
+		$this->textValide = $name;
+
+	}
+
+	/*
+	*Проверка строки на наличие экранирующих символов
+	*/
+	protected function testInput($data) {
+
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+	/*
+	*Верификация строки со скобками
+	*/
+	protected function valideString($string){
+
+		$counter = 0;
+		$openBracket = ['(','{','['];
+		$closedBracket = [')','}',']'];
+
+		$length = strlen($string);
+
+		for($i = 0; $i<$length; $i++){
+			$char = $string[$i];
+
+			if(in_array($char, $openBracket)){
+				$counter ++;
+			}elseif(in_array($char, $closedBracket)){
+				$counter --;
+			}
+
+			if($counter<0){
+				break;
+			}
+		}
+
+		if($counter != 0) { return false;}
+		
+		return true;
+	}
+
+	public function Index(){
+
+		if($this->valideString($this->textValide)==true){
+			$res = '200';
+		}else{
+			$res = '400';
+		}
+
+		header("Location: result.php?res=$res");
+
+		return true;	
+	}	
+}/*End Class*/
+
+
+/*
+*Проверяем поля формы на пустоту
+*/
+if(isset($_POST['form-id']) && !empty($_POST['form-id'])){
+
+	if(isset($_POST['str-form']) && !empty($_POST['str-form'])){
+
+		$formValide = new FormValide($_POST['str-form']); 
+		$formValide->Index();
+
+		exit;
+	}else{
+		header('HTTP/1.1 400 Bad Request');
+		$textError = '<span class="text-error">Поле со строкой не заполнено!</span>';
+	}
+}
---
+++ code/index.php
@@ -0,0 +1,33 @@
+<?php
+	include_once 'action-form.php';
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<div class="contain">
+		<form action="#" id="ajax-form" class="form-gs contain__form-gs" method="post">
+			<input type="hidden" name="form-id" value="form-contact">
+			<div class="title-form">Введите строку</div>
+			<div class="form-gs__item">
+				<label for="str-form" class="label-form">Строка с (): </label><input type="text" class="input-form"
+					name="str-form" value="">
+			</div>
+			<div class="form-gs__item">
+				<button class="but-send">Отправить</button>
+			</div>
+		</form>
+		<br>
+		<div class="result-form"><?php echo $textError;?></div>
+	</div>
+</body>
+
+</html>
