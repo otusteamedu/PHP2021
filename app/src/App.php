5d908b59d4065e408824b57b2ffc196137f30eb0
Aleksandr Chuklin <a.chuklin@notamedia.ru>
2021-11-15 18:04:53 +0300

    fix

--- app/src/App.php
+++ app/src/App.php
@@ -7,17 +7,14 @@ class App
 
     public function run(): void
     {
-        $string = $_POST['string'];
-        if (!empty($string) && (strpos($string,')') > strpos($string,'('))
-            && (strrpos($string,'(') < strrpos($string,')'))) {
-            $countBrackets = array_count_values(str_split($string));
-            if ($countBrackets['('] === $countBrackets[')']) {
-                header('HTTP/1.0 200 Ok');
-                echo '200 Ok';
-                exit();
+        try {
+            if (Validator::String($_POST['string'])) {
+                Response::success();
+            } else {
+                Response::fail();
             }
+        } catch (\Exception $e) {
+            Response::fail();
         }
-        header('HTTP/1.0 400 Bad Request');
-        echo '400 Bad Request';
     }
 }
