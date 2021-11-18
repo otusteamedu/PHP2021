1599135f94eaec6682ed3adb52be3ed4bfaf2f4a
Александр Васечкин <administrator@MacBook-Pro-Aleksandr.local>
2021-11-18 10:11:24 +0300

    Сервис проверки email

---
+++ homework/5/email_verificator/code/src/EmailCheckerService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+class EmailCheckerService
+{
+    public static function checkEmailByRegExpression(string $emailAddress): bool
+    {
+        preg_match('/(\S+)@([a-z0-9.]+)/is', $emailAddress, $matchList);
+
+        return isset($matchList[0]);
+    }
+
+    public static function checkEmailByMX(string $emailAddress): bool
+    {
+        $p = stripos($emailAddress, '@');
+
+        if ($p === false) {
+            return false;
+        }
+
+        $host = substr($emailAddress, $p + 1);
+
+        getmxrr($host, $r);
+
+        return isset($r[0]);
+    }
+
+    public static function checkEmail(string $emailAddress): bool
+    {
+        return (   self::checkEmailByRegExpression($emailAddress)
+                && self::checkEmailByMX($emailAddress)
+        );
+    }
+}
