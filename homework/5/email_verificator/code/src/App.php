96dd0668f0f5946e11324105e573d6294bc73c10
Александр Васечкин <administrator@MacBook-Pro-Aleksandr.local>
2021-11-18 10:36:16 +0300

    Класс App

---
+++ homework/5/email_verificator/code/src/App.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use CommonHelpers\ObjectResponse\Response;
+
+class App
+{
+    private $response;
+
+    public function __construct()
+    {
+        $this->response = Response::success();
+    }
+
+    public function run()
+    {
+        $emailAddressList = [
+            'aaaavs@yandex.ru',
+            'andfbne@domen-domenovich.zone',
+        ];
+
+        $checkResult = EmailCheckerService::checkEmailList($emailAddressList);
+
+        if ($checkResult === true) {
+            $this->response
+                ->setResult($checkResult)
+                ->setMessage('All addresses is correct')
+            ;
+        } else {
+            $this->response
+                ->setResult($checkResult)
+                ->setMessage(sprintf('Address %s is incorrect', $checkResult))
+            ;
+        }
+
+        return $this->response;
+    }
+}
