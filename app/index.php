c4df7ff9c5eaad41dedf0d0a5de5d3af89dc0e7d
Aleksandr Chuklin <a.chuklin@notamedia.ru>
2021-11-19 11:03:25 +0300

    fix

--- app/index.php
+++ app/index.php
@@ -1,7 +1,10 @@
 <?php
-use Src\App;
-
 require_once 'vendor/autoload.php';
+use Src\App;
+try {
+    $app = new App();
+    $app->run();
+} catch (Exception $e) {
+    echo 'The app is not work';
+}
 
-$app = new App();
-$app->run();
