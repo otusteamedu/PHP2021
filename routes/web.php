1b6473702d1afea0b6c87c946243a5422936df50
Царев Дмитрий Борисович <d.b.tsarev@ticketland.ru>
2021-11-23 13:11:26 +0300

    hw10

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\EventAddController;
+use App\Http\Controllers\EventClearController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\EventFindController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', EventController::class);
+Route::get('/clear', EventClearController::class);
+Route::post('/findEven', EventFindController::class);
+Route::post('/addEven', EventAddController::class);
+
