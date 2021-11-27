2fc2285887cb23efd0118d0f89aff08b0d50dfd1
raketa_nickolay <hugopochta@gmail.com>
2021-11-28 02:28:46 +0300

    'proccess'

---
+++ improves.sql
@@ -0,0 +1,19 @@
+--
+-- Name: name; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX name ON public.films USING btree (name);
+
+
+--
+-- Name: name_len; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX name_len ON public.films USING btree (length((name)::text));
+
+
+--
+-- Name: price; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX price ON public.sessions USING btree (price);
--- seads/sead10000000.sql
+++ seads/sead10000000.sql
@@ -1,53 +1,53 @@
 BEGIN;
     -- films
-INSERT INTO "public"."films" ("id", "name")
-SELECT 1, 'Мстители';
+    INSERT INTO "public"."films" ("id", "name")
+    SELECT 1, 'Мстители';
 
-INSERT INTO "public"."films" ("id", "name")
-SELECT gs.id, random_string(10) FROM generate_series(2, 10000000) as gs(id);
+    INSERT INTO "public"."films" ("id", "name")
+    SELECT gs.id, random_string(10) FROM generate_series(2, 10000000) as gs(id);
 
--- halls
+    -- halls
 
-INSERT INTO "public"."halls" ("id", "name")
-SELECT gs.id, random_string(10) FROM generate_series(1, 10000000) as gs(id);
+    INSERT INTO "public"."halls" ("id", "name")
+    SELECT gs.id, random_string(10) FROM generate_series(1, 10000000) as gs(id);
 
--- hall zones
+    -- hall zones
 
-INSERT INTO "public"."hall_zones" ("id", "hall_id", "name")
-SELECT gs.id,
-       (random() * gs.id + 1)::int,
-        random_string(10) FROM generate_series(1, 10000000) as gs(id);
+    INSERT INTO "public"."hall_zones" ("id", "hall_id", "name")
+    SELECT gs.id,
+           (random() * gs.id + 1)::int,
+            random_string(10) FROM generate_series(1, 10000000) as gs(id);
 
--- sessions
+    -- sessions
 
-INSERT INTO "public"."sessions" ("id", "film_id", "hall_zone_id", "price", "time")
-SELECT gs.id,
-       (random() * gs.id + 1)::int,
-        (random() * gs.id + 1)::int,
-            gs.id + random() * 10 + 1,
-       '14:00'
-FROM generate_series(1, 10000000) as gs(id);
+    INSERT INTO "public"."sessions" ("id", "film_id", "hall_zone_id", "price", "time")
+    SELECT gs.id,
+           (random() * gs.id + 1)::int,
+            (random() * gs.id + 1)::int,
+                gs.id + random() * 10 + 1,
+           '14:00'
+    FROM generate_series(1, 10000000) as gs(id);
 
---seats
+    --seats
 
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 1, 1 FROM generate_series(1, 1000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 1, 2 FROM generate_series(1000001, 2000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 1, 3 FROM generate_series(2000001, 3000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 1, 4 FROM generate_series(3000001, 4000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 1, 5 FROM generate_series(4000001, 5000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 2, 1 FROM generate_series(5000001, 6000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 2, 2 FROM generate_series(6000001, 7000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 2, 3 FROM generate_series(7000001, 8000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 2, 4 FROM generate_series(8000001, 9000000) as gs(id);
-INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
-SELECT gs.id, gs.id, 2, 5 FROM generate_series(9000001, 10000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 1, 1 FROM generate_series(1, 1000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 1, 2 FROM generate_series(1000001, 2000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 1, 3 FROM generate_series(2000001, 3000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 1, 4 FROM generate_series(3000001, 4000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 1, 5 FROM generate_series(4000001, 5000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 2, 1 FROM generate_series(5000001, 6000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 2, 2 FROM generate_series(6000001, 7000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 2, 3 FROM generate_series(7000001, 8000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 2, 4 FROM generate_series(8000001, 9000000) as gs(id);
+    INSERT INTO "public"."seats" ("id", "hall_zone_id", "row", "seat")
+    SELECT gs.id, gs.id, 2, 5 FROM generate_series(9000001, 10000000) as gs(id);
 COMMIT
