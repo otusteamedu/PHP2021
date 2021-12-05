c5e271bd9fd9e6f07150b503a5a10a65f09db06f
raketa_nickolay <hugopochta@gmail.com>
2021-12-05 18:15:20 +0300

    'commit'

---
+++ code/app/Repositories/Redis.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Repositories;
+
+class Redis
+{
+    public function index()
+    {
+        return 123;
+    }
+}
---
+++ code/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '732297091f9af979d5bc7c77eabb7ef91d37c31d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '732297091f9af979d5bc7c77eabb7ef91d37c31d',
+    ),
+    'predis/predis' => 
+    array (
+      'pretty_version' => 'v1.1.9',
+      'version' => '1.1.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c50c3393bb9f47fa012d0cdfb727a266b0818259',
+    ),
+  ),
+);
