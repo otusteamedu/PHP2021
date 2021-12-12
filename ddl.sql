fff4203721eb5d9577c12810c0d63d46bf3ad964
Виктор Бурьянов <viktorburanov@MacBook-Air-Viktor.local>
2021-12-12 18:37:14 +0300

    homework 7

---
+++ ddl.sql
@@ -0,0 +1,58 @@
+
+CREATE TABLE halls (
+	id SERIAL,
+	name VARCHAR(150),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE films (
+	id SERIAL,
+	name VARCHAR(120),
+	price NUMERIC(5,2) NOT NULL,
+	duration INT NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE tariff (
+	id SERIAL,
+	name VARCHAR(120) NOT NULL,
+	ratio NUMERIC(3,2) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE session (
+	id SERIAL,
+	hall_id INT NOT NULL,
+	films_id INT NOT NULL,
+	tariff_id INT NOT NULL,
+	start_time TIME NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(hall_id) REFERENCES halls(id),
+	FOREIGN KEY(films_id) REFERENCES films(id),
+	FOREIGN KEY(tariff_id) REFERENCES tariff(id)
+);
+
+CREATE TABLE seats (
+	id SERIAL,
+	row INT  NOT NULL,
+	seat INT NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE users (
+	id SERIAL,
+	name VARCHAR(220) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE tickets (
+	id SERIAL,
+	user_id INT NOT NULL,
+	seat_id INT NOT NULL,
+	session_id INT NOT NULL,
+	total_price NUMERIC(5,2) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(seat_id) REFERENCES seats(id),
+	FOREIGN KEY(session_id) REFERENCES session(id)
+);
