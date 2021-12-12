681eb5e5a56a5ed28847e66c0ef564b1902edf04
raketa_nickolay <hugopochta@gmail.com>
2021-12-12 11:28:21 +0300

    'start'

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => 3306,
+  'DB_NAME' => 'db',
+  'DB_USER' => 'root',
+  'DB_PASSWORD' => ''
+];
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+require '../vendor/autoload.php';
+require '../config/db.php';
