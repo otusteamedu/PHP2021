1d807c427a05d6a5167d4976ecee43e4a68ae48e
raketa_nickolay <hugopochta@gmail.com>
2021-12-25 14:12:27 +0300

    'fix'

--- image.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Application\Services\Config;
-
-header("Content-type: image/jpg");
-$fileId = (int)$_GET["id"];
-//$data = file_get_contents(PROJECT_PATH . "/images/" . $fileId . ".jpg");
-$data = file_get_contents(Config::getApp('PROJECT_PATH') . "/images/" . 140 . ".jpg");
-echo $data;
