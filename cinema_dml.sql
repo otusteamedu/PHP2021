5eee68c099dc9bca34834ae2b4cfc52381affd89
Mikhail <sccinet@gmail.com>
2021-12-26 19:09:05 +0500

    Change sql-scripts

--- cinema_dml.sql
+++ cinema_dml.sql
@@ -1,16 +1,31 @@
 -- Поиск самого прибыльного фильма
 WITH
     movie_profit_ranks AS (
+        WITH
+            session_profit AS (
+                SELECT
+                    session_id,
+                    SUM(price) AS profit
+                from
+                    cinema_tickets
+                GROUP BY
+                    session_id, status
+                HAVING
+                    status = 1)
         SELECT
+            cs.movie_id,
             cm.name_original AS movie_name,
-            rank() OVER (ORDER BY SUM(cs.ticket_price*cs.sold_seats) DESC) AS rnk
+            rank() OVER (ORDER BY SUM(profit) desc) AS rnk
         FROM
             cinema_sessions AS cs
+                JOIN session_profit AS sp ON
+                sp.session_id = cs.id
                 JOIN cinema_movies AS cm ON
                 cm.id = cs.movie_id
         GROUP BY
             cs.movie_id,
-            cm.name_original)
+            cm.name_original
+    )
 SELECT
     movie_name
 FROM
