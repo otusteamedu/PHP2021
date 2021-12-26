7beae2a535b4b432038804f73bed5e03e026605e
valeriy <valeriybaranov.elitel@gmail.com>
2021-12-26 17:55:16 +0300

    добавить возможность установки пакета dc

--- sum.sh
+++ sum.sh
@@ -12,5 +12,37 @@ if ! [[ $1 =~ $re ]] | [[ $2 =~ $re ]] ; then
     exit 1
 fi
 
-result=$(echo "$1 $2 + p" | dc)
+lst="dc"
+
+for package in $lst
+do
+  cmd=$(dpkg --status $package 2>/dev/null | grep "ok installed")
+  if [ $? != 0 ]
+    then
+
+      echo -n "Для продолжения необходимо уствновить пакет «$package», продолжить? (y/n) "
+      read item
+      case "$item" in
+          y|Y) echo "Вы ввели «y», продолжаем..."
+              echo "Установка пакета «$package»"
+              apt-get update
+              sudo apt-get install $package
+              if [ "$?" = "0" ];
+                  then echo Пакет "«$package»" успешно установлен.
+              fi
+              ;;
+          n|N) echo "Вы ввели «n», завершаем..."
+              exit 0
+              ;;
+          *) echo "Для установки пакета «$package» нужно ввести «y», завершаем.."
+              exit 0
+              ;;
+      esac
+
+
+
+  fi
+done
+
+result=$(echo "Результат: $1 $2 + p" | dc)
 echo "$1+$2=$result"
