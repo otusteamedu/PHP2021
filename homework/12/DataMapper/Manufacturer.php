0e8a6d927b062220d81101aecce09145c6d6e5ed
Alex Vasechkin <vasechkin.alex.v@gmail.com>
2022-01-10 00:55:32 +0300

    Класс-сущность 'Производитель'

---
+++ homework/12/DataMapper/Manufacturer.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vasechkin\DataMapper;
+
+class Manufacturer
+{
+    private $id;
+
+    private $code;
+
+    private $name;
+
+    public function __construct(int $id = null, string $code = null, string $name = null)
+    {
+        $this->id = $id;
+        $this->code = $code;
+        $this->name = $name;
+    }
+
+	public function getId(): ?int
+    {
+		return $this->id;
+	}
+
+	public function setId(?int $id): self
+    {
+		$this->id = $id;
+        return $this;
+	}
+
+	public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): self
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
