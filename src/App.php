115786013c739367a86447232dbb070a527df76b
raketa_nickolay <hugopochta@gmail.com>
2022-01-10 23:58:19 +0300

    'finish'

--- src/App.php
+++ src/App.php
@@ -7,25 +7,49 @@ use App\Application\Strategies\BurgerStrategy;
 use App\Application\Strategies\HotDogStrategy;
 use App\Application\Strategies\SandwichStrategy;
 use App\Application\Strategies\Strategy;
+use App\Application\Visitors\Visitor;
+use App\Domain\VisitorInterface;
+use App\Infrastructure\Factories\ItalianProductFactory;
+use function DI\factory;
 
 class App
 {
     private ProductFactoryInterface $productFactory;
     private Strategy $productStrategy;
     private $strategies;
+    public $container;
 
     public function __construct()
     {
-        global $container;
+        $this->setContainer();
+        $this->setStrategies();
+    }
+
+    private function setContainer()
+    {
+        $builder = new \DI\ContainerBuilder();
+        $builder->addDefinitions([
+            ProductFactoryInterface::class => factory(function () {
+                return new ItalianProductFactory();
+            }),
+            VisitorInterface::class => factory(function () {
+                return new Visitor();
+            }),
+        ]);
+        $this->container = $builder->build();
+    }
+
+    private function setStrategies()
+    {
         $this->strategies = [
-            'burger' => function () use ($container){
-                return $container->make(BurgerStrategy::class);
+            'burger' => function () {
+                return $this->container->make(BurgerStrategy::class);
             },
-            'hotdog' => function () use ($container){
-                return $container->make(HotDogStrategy::class);
+            'hotdog' => function () {
+                return $this->container->make(HotDogStrategy::class);
             },
-            'sandwich' => function () use ($container){
-                return $container->make(SandwichStrategy::class);
+            'sandwich' => function () {
+                return $this->container->make(SandwichStrategy::class);
             },
         ];
     }
