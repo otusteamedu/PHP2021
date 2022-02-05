c526c669fa562d885d6434689c320fd16dd03352
JastaFly <zumawuu@gmail.com>
2022-02-05 21:25:32 +0300

    HM-2/Summation bash script

---
+++ summation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+digitalCheckReg='^[+-]?[0-9]+([.][0-9]+)?$'
+
+if [ $# -lt 2 ]
+  then
+    echo 'Нужно передать 2 аргумента'
+    exit 1
+fi
+
+if ! [[ $1 =~ $digitalCheckReg ]]
+then
+   echo 'Первый агрумент не является числом'
+   exit 1
+elif ! [[ $2 =~ $digitalCheckReg ]]
+then
+  echo 'Второй аргумент не является числом'
+  exit 1
+fi
+
+sum=$(( $1 + $2 ))
+
+echo "Сумма равна: $sum"
