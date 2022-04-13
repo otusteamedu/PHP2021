6f2df64f8fe0a43e9e4ff43e8cdde74633d05eb2
Victor <52466770+Vivchy@users.noreply.github.com>
2022-04-13 12:38:26 +0300

    new ref

--- new/Check.php
+++ new/Check.php
@@ -14,14 +14,19 @@ class Check
     {
         $arChars = str_split($request);
         $openBracketsCounter = 0;
-        foreach($arChars AS $singleChar) {
-            if($openBracketsCounter <= 0) throw new \Exception('Brackets not paired');
-
-            if ($singleChar == self::$closeBracket) $openBracketsCounter--;
-            else if ($singleChar == self::$openBracket) $openBracketsCounter++;
+        foreach ($arChars AS $singleChar) {
+            if ($openBracketsCounter <= 0) {
+                throw new \Exception('Brackets not paired');
+            }
+            if ($singleChar == self::$closeBracket) {
+                $openBracketsCounter--;
+            } elseif ($singleChar == self::$openBracket) {
+                $openBracketsCounter++;
+            }
         }
-        if ($openBracketsCounter) throw new \Exception('Brackets not paired');
-
+        if ($openBracketsCounter) {
+            throw new \Exception('Brackets not paired');
+        }
         throw new \Exception(self::MESSAGE);
     }
 }
