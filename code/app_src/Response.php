32013a104b6a4ad45f42a9a4d78ac0aa3578f0f6
zumawuu <zumawuu@gmail.com>
2022-05-24 20:43:37 +0300

    HW-6/Correct method getErrorMessage

--- code/app_src/Response.php
+++ code/app_src/Response.php
@@ -4,6 +4,16 @@ namespace App;
 
 class Response
 {
+    private static array $arErrors = [
+        'ERROR_REQUEST_METHOD' => 'Ошибочный метод запроса :(',
+        'EMPTY_REQUEST' => 'Пустой запрос :/',
+        'EMPTY_INPUT' => 'В поле ничего не введено :|',
+        'DATA_NO_GENERATED' => 'Данные не сгенерированы o_0',
+        'EMAIL_OK' => 'Адрес валиден и MX-запись найдена :)',
+        'EMAIL_MX_FAILED' => 'MX запись для адреса, не найдено :(',
+        'EMAIL_VALID_FAILED' => 'Адрес не прошёл валидацию :('
+    ];
+
     public static function generateOkResponse(string $code)
     {
         header('HTTP/1.0 200 Ok');
@@ -20,45 +30,6 @@ class Response
 
     private static function getErrorMessage(string $errorCode): string
     {
-        switch ($errorCode) {
-
-            case 'ERROR_REQUEST_METHOD':
-                return 'Ошибочный метод запроса :(';
-
-                break;
-
-            case 'EMPTY_REQUEST':
-                return 'Пустой запрос :/';
-
-                break;
-
-            case 'EMPTY_INPUT':
-                return 'В поле ничего не введено :|';
-
-                break;
-
-            case 'DATA_NO_GENERATED':
-                return 'Данные не сгенерированы o_0';
-
-                break;
-
-            case 'EMAIL_OK':
-                return "Адрес валиден и MX-запись найдена :)";
-
-                break;
-
-            case 'EMAIL_MX_FAILED':
-                return "MX запись для адреса, не найдено :(";
-
-                break;
-
-            case 'EMAIL_VALID_FAILED':
-                return "Адрес не прошёл валидацию :(";
-
-                break;
-
-            default:
-                return 'Неизвестная ошибка 0_0';
-        }
+        return self::$arErrors[$errorCode];
     }
 }
